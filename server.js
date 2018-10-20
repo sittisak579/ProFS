d2899d73e44198e0e43fb2e20710a8eb499aa61c
Jirawat <jirawat_pao@hotmail.co.th>
2018-10-20 10:03:12 +0700

    update ADDP

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var pgp = require('pg-promise')();
-var db = pgp('postgres://nkwnjxuiidwrns:b72b4de42f726173c9acee8a85dd10ed1c8dc1a2ab7402a6feebbbccb8b14f85@ec2-54-163-245-44.compute-1.amazonaws.com:5432/d34ii1v5fr4h1e?ssl=true');
+// var db = pgp(process.env.DATABASE_URL);
+var db = pgp('postgres://vxjopouelszoqp:b38107853d55cfcfb5ec84d98b68feb33b5b5dad7fae2fa12a9484f47bad97d1@ec2-54-243-147-162.compute-1.amazonaws.com:5432/d28vk975k8u2um?ssl=true');
 var app =express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());

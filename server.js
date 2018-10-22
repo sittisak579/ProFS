afb6eeab75a79f72774ecebcfeb3f7c57be38b44
Jirawat <jirawat_pao@hotmail.co.th>
2018-10-22 07:44:30 +0700

    90%

--- server.js
+++ server.js
@@ -236,7 +236,7 @@ app.get('/report_products', function (req, res) {
 });
 
 app.get('/report_users', function (req, res) {
-    var sql = 'select users.email,purchases.name,products.title,purchase_items.quantity,purchase_items.price*purchase_items.quantity as total FROM users INNER JOIN purchases ON purchases.user_id = users.user_id INNER JOIN purchase_items ON purchase_items.purchase_id=purchases.purchase_id   INNER JOIN products ON products.product_id = purchase_items.product_id order by purchase_items.price*purchase_items.quantity DESC limit 25';
+    var sql = 'select users.email,purchases.name,products.title,purchase_items.quantity,purchase_items.price*purchase_items.quantity as total FROM users INNER JOIN purchases ON purchases.user_id = users.user_id INNER JOIN purchase_items ON purchase_items.purchase_id=purchases.purchase_id   INNER JOIN products ON products.product_id = purchase_items.product_id order by purchase_items.price*purchase_items.quantity DESC limit 20';
     db.any(sql)
         .then(function (data) {
             console.log('DATA:' + data);
